efc3d18d70781abd1cc3fa7af206de0decca9081
Kenny Shi <kenshi@ucla.edu>
2015-05-12 17:20:51 -0700

    Changed UI, Added splash screen logo, main screen logo, tested with Nexus S

--- app/src/main/java/edu/ucla/csm117/bluetoothattendance/MainActivity.java
+++ app/src/main/java/edu/ucla/csm117/bluetoothattendance/MainActivity.java
@@ -1,5 +1,6 @@
 package edu.ucla.csm117.bluetoothattendance;
 
+
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -14,6 +15,7 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
     }
 
